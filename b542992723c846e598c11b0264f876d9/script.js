59db630f89e9773b8ece97f003af6e78ae76f152
generaloriginal <155470475+generaloriginal@users.noreply.github.com>
2025-01-01 19:00:40 -0500

    None

--- b542992723c846e598c11b0264f876d9/script.js
+++ b542992723c846e598c11b0264f876d9/script.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
         { word: "Avocado", img: "Avocado (ah-vuh-CAH-doh).jpeg" },
         { word: "Blackberry", img: "Blackberry (bl-ack-ber-ree).jpeg" },
         { word: "Blackberry", img: "Blackberry (bl-ack-ber-ree).jpeg" },
-        { word: "Blueberry", img: "Blueberry.jpeg" },
-        { word: "Blueberry", img: "Blueberry.jpeg" },
-        { word: "Cherry", img: "Cherry.jpeg" },
-        { word: "Cherry", img: "Cherry.jpeg" }
+        { word: "Avocado", img: "Avocado (ah-vuh-CAH-doh).jpeg" },
+        { word: "Avocado", img: "Avocado (ah-vuh-CAH-doh).jpeg" },
+        { word: "Blackberry", img: "Blackberry (bl-ack-ber-ree).jpeg" },
+        { word: "Blackberry", img: "Blackberry (bl-ack-ber-ree).jpeg" },
     ];
 
     // Shuffle function (Fisher-Yates Shuffle)
